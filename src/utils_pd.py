066c8a99544864651260c1413aca9b381755f80f
tsepanx <77626319+tsepanx@users.noreply.github.com>
2023-03-06 16:25:16 +0300

    dot not remove `|` like char

--- src/utils_pd.py
+++ src/utils_pd.py
@@ -17,7 +17,7 @@ def df_to_markdown(df: pd.DataFrame, transpose=False):
             "\U0001F300-\U0001F5FF"  # symbols & pictographs
             "\U0001F680-\U0001F6FF"  # transport & map symbols
             "\U0001F1E0-\U0001F1FF"  # flags (iOS)
-            "\U00002500-\U00002BEF"  # chinese char
+            "\U00002503-\U00002BEF"  # chinese char
             "\U00002702-\U000027B0"
             "\U00002702-\U000027B0"
             "\U000024C2-\U0001F251"
