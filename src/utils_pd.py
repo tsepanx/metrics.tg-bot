a81a2b5d67f55905eee62046247ad0c00f8288d8
tsepanx <77626319+tsepanx@users.noreply.github.com>
2023-03-06 15:50:30 +0300

    remove emojis in image markdown string

--- src/utils_pd.py
+++ src/utils_pd.py
@@ -1,4 +1,5 @@
 import datetime
+import re
 
 import numpy as np
 import pandas as pd
@@ -9,6 +10,32 @@ from src.tables.question import (
 
 
 def df_to_markdown(df: pd.DataFrame, transpose=False):
+    def remove_emojis_with_space_prefix(data: str) -> str:
+        emojis_regex = re.compile(
+            " ["
+            "\U0001F600-\U0001F64F"  # emoticons
+            "\U0001F300-\U0001F5FF"  # symbols & pictographs
+            "\U0001F680-\U0001F6FF"  # transport & map symbols
+            "\U0001F1E0-\U0001F1FF"  # flags (iOS)
+            "\U00002500-\U00002BEF"  # chinese char
+            "\U00002702-\U000027B0"
+            "\U00002702-\U000027B0"
+            "\U000024C2-\U0001F251"
+            "\U0001f926-\U0001f937"
+            "\U00010000-\U0010ffff"
+            "\u2640-\u2642"
+            "\u2600-\u2B55"
+            "\u200d"
+            "\u23cf"
+            "\u23e9"
+            "\u231a"
+            "\ufe0f"  # dingbats
+            "\u3030"
+            "]+",
+            re.UNICODE,
+        )
+        return re.sub(emojis_regex, "", data)
+
     if transpose:
         df = df.T
 
@@ -21,6 +48,8 @@ def df_to_markdown(df: pd.DataFrame, transpose=False):
         stralign="left",
     )
     text = text.replace(" nan ", " --- ")
+
+    text = remove_emojis_with_space_prefix(text)
 
     # text = text.replace("00:00:00", "0       ")
     # text = text.replace(":00:00", ":0c0   ")
